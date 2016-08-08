8da1e1d48265a86adfd02947ce25a666e12fdd6e
anortec <kenrellinux@kernellinux.net>
2016-08-07 20:20:21 -0400

    final push for now

---
+++ code_examples/ifstatement.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+value = 110
+
+if value == 10:
+
+# The action must be indented
+
+	print "This statement only prints if the above is true"
+
+
+
+print "this statement always prints"
+
