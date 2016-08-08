e0ba03a8eadc0872ee4edacdf1a28d5ea4c63b91
anortec <kenrellinux@kernellinux.net>
2016-08-08 14:19:20 -0400

    note

---
+++ code_examples/askdivisible.py
@@ -0,0 +1,4 @@
+number = int(raw_input("Fuckin number brah? "))
+
+if 100 % number == 0:
+	print "divisible/10"
---
+++ code_examples/testdivisible.py
@@ -0,0 +1,13 @@
+# You do this using the modulus operator, %
+
+# n % k == 0
+
+# evaluates true if and only if n is an exact multiple of k. In elementary maths this is known as the remainder from a division.
+
+
+
+if 15 % 5 == 0:
+	print "it is divisble"
+
+
+5 % 10 == 0
